8e60f8a8898c24a5eafad7ac6fe7b7bc0c31943c
weijia su <32049018+WeijiaSu@users.noreply.github.com>
2024-08-20 11:06:07 -0700

    Delete scripts/saw8 directory

--- scripts/saw8/Build_index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-saw makeRef \
-    --mode=STAR \
-    --fasta=Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-    --gtf=Homo_sapiens.GRCh38.93.gtf \
-    --genome=./Homo_sapiens.GRCh38.93.index
